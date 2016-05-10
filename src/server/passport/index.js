7a14a27643ba78754782786101e6b06e9f62cc7b
Alejo Moreno <tecksonic@gmail.com>
2016-05-10 08:20:42 -0500

    Add Facebook Auth & change buttons look

--- src/server/passport/index.js
+++ src/server/passport/index.js
@@ -4,18 +4,18 @@ import {getUserById} from 'src/server/users';
 import config from 'config';
 
 var TwitterStrategy = require('passport-twitter').Strategy;
-//var FacebookStrategy = require('passport-facebook').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var passportConfig = config.get('development.passportConfig');
 
 
-export default function (passport) {
+export default function(passport) {
 
     passport.serializeUser((user, done) => {
         done(null, user.provider_id)
     });
 
-    passport.deserializeUser(function (id, done) {
-        getUserById(id, function (err, user) {
+    passport.deserializeUser(function(id, done) {
+        getUserById(id, function(err, user) {
             done(null, user);
         });
     });
@@ -24,8 +24,8 @@ export default function (passport) {
         consumerKey: passportConfig.TWITTER_CONSUMER_KEY,
         consumerSecret: passportConfig.TWITTER_CONSUMER_SECRET,
         callbackURL: '/auth/twitter/callback'
-    }, function (accessToken, refreshToken, profile, done) {
-        User.findOne({ "provider_id": profile.id }, function (err, user) {
+    }, function(accessToken, refreshToken, profile, done) {
+        User.findOne({ "provider_id": profile.id }, function(err, user) {
             debugger;
             if (err) done(err);
             if (!err && user != null) return done(null, user);
@@ -38,7 +38,7 @@ export default function (passport) {
                 createdAt: new Date()
             });
 
-            user.save(function (err) {
+            user.save(function(err) {
                 if (err) throw err;
                 done(null, user);
             });
@@ -47,25 +47,27 @@ export default function (passport) {
     }));
 
 
-    /* passport.use(new FacebookStrategy({
-         clientID: process.env.FACEBOOK_APP_CLIENT_ID,
-         clientSecret: process.env.FACEBOOK_APP_SECRET_ID,
-         callbackURL: '/auth/facebook/callback'
-     }, function (accessToken, refreshToken, profile, done) {
-         User.findOne({ provider_id: profile.id }, function (err, user) {
-             if (err) throw (err);
-             if (!err && user != null) return done(null, user);
- 
-             var user = new User({
-                 provider_id: profile.id,
-                 provider: profile.provider,
-                 name: profile.displayName,
-                 photo: profile.photos[0].value
-             });
-             user.save(function (err) {
-                 if (err) throw err;
-                 done(null, user);
-             });
-         });
-     }));*/
+    passport.use(new FacebookStrategy({
+        clientID: passportConfig.FACEBOOK_APP_CLIENT_ID,
+        clientSecret: passportConfig.FACEBOOK_APP_SECRET_ID,
+        callbackURL: '/auth/facebook/callback',
+        profileFields: ['id', 'displayName', 'name', 'photos']
+    }, function(accessToken, refreshToken, profile, done) {
+        User.findOne({ provider_id: profile.id }, function(err, user) {
+            if (err) throw (err);
+            if (!err && user != null) return done(null, user);
+
+            var user = new User({
+                provider_id: profile.id,
+                provider: profile.provider,
+                name: profile.displayName,
+                photo: profile.photos ? profile.photos[0].value : '',
+                createdAt: new Date()
+            });
+            user.save(function(err) {
+                if (err) throw err;
+                done(null, user);
+            });
+        });
+    }));
 }
